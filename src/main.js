8587702a23e8332a879b24a4b8b234836b1d42fa
liangdongsheng <354972463@qq.com>
2020-04-26 23:47:06 +0800

    tab导航，对应的4个页面，和路由配置，app默认主题色，默认选中路由高亮，tab属性渲染标签

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill' // 对es6的api进行转义 如：promiss
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import fastclick from 'fastclick' // 解决移动端300毫秒的延迟
 
 import 'common/stylus/index.styl'
@@ -11,5 +11,6 @@ fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,27 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-
 export default new Router({
   routes: [
-
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      component: () => import('components/recommend/recommend')
+    },
+    {
+      path: '/singer',
+      component: () => import('components/singer/singer')
+    },
+    {
+      path: '/rank',
+      component: () => import('components/rank/rank')
+    },
+    {
+      path: '/search',
+      component: () => import('components/search/search')
+    }
   ]
 })
