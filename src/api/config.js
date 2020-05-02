edc93838a4a86807145554477ddee3bedde1d05c
liangdongsheng <354972463@qq.com>
2020-05-02 15:52:18 +0800

    抓取轮播图数据，失效

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export const commonParams = {
+    g_tk: 5381,
+    format: 'json',
+    inCharset: 'utf8',
+    outCharset: 'GB2312',
+    notice: 0,
+}
+
+export const options = {
+    param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
