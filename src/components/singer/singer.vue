4ec76a07cb7037f7f477bdbf20260a9f3dbc4ae6
liangdongsheng <354972463@qq.com>
2020-05-08 08:53:33 +0800

    获取歌手数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,40 @@
 <template>
- <div>
-   歌手
- </div>
+  <div class="singer" ref="singer">
+
+  </div>
 </template>
 
-<script type="text/ecmascript-6">
+<script>
+import { getSingerList } from "api/singer";
+import { ERR_OK } from "api/config";
+
+export default {
+  data() {
+    return {
+      singers: []
+    };
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    // 歌手数据
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list;
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
- 
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
