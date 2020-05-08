8c95d9b2ad89cd3f198efbb89241b6f326408001
liangdongsheng <354972463@qq.com>
2020-05-08 08:03:15 +0800

    loading的封装使用

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -28,19 +28,24 @@
           </ul>
         </div>
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 
 <script type="text">
 import Slider from 'base/slider/slider';
+import Loading from 'base/loading/loading'
 import Scroll from 'base/scroll/scroll'
 import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 export default {
   components: {
     Slider,
-    Scroll
+    Scroll,
+    Loading
   },
   data() {
     return {
