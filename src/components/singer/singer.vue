a85b52db25e42f7c61320a60192cf55c6c9f5817
liangdongsheng <354972463@qq.com>
2020-05-13 08:40:54 +0800

    有序化歌手列表，类的封装使用，字母排序(有参考意义)

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,7 +7,10 @@
 <script>
 import { getSingerList } from "api/singer";
 import { ERR_OK } from "api/config";
+import Singer from "../../common/js/singer";
 
+const HOT_NAME = '热门';
+const HOT_SINGER_LEN = 10;
 export default {
   data() {
     return {
@@ -22,9 +25,56 @@ export default {
     _getSingerList() {
       getSingerList().then(res => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list;
+          this.singers = this._normalizeSinger(res.data.list);
+          console.log(this.singers,8888);
+          
         }
       });
+    },
+    // 格式化歌手数据
+    _normalizeSinger(list) {
+      // 定义热门数据
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item,index) => {
+        // 获取返回的的前几条数据作为热门数据
+        if(index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name,
+          }));
+        }
+        // 如果对象里面这个key(字母)不存在，则创建一个对象
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        // 在已创建的key里面，放入相应的歌手数据
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name,
+        }))
+      })
+       // 为了得到有序列表，我们需要处理 map
+      let hot = [];
+      let ret = [];
+      for(let key in map) {
+        let val = map[key];
+        if(val.title.match(/[a-zA-z]/)) {
+          ret.push(val);
+        }else if(val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a,b) => a.title.charCodeAt(0) - b.title.charCodeAt(0));
+      return hot.concat(ret);
     }
   }
 };
